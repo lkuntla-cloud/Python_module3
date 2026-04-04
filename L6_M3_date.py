07508e88883ce6d986cbccb139db36f3a21bebbd
Divit Reddy Allu <lkuntla@googlemail.com>
2026-04-04 11:22:55 +0200

    Lesson 6

---
+++ L6_M3_date.py
@@ -0,0 +1,7 @@
+from datetime import date,time,datetime
+today=date.today()
+now=datetime.now()
+print("Todays date is: ",today)
+print("right now the time is: ",now)
+print("date components",today.year,today.month,today.day)
+
